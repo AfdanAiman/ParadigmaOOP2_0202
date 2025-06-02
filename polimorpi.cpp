05bd9a829eaffbf181bcfdaf88c5345447fb3913
AfdanAiman <afdanaiman06@gmail.com>
2025-06-02 14:25:43 +0700

    menambahkan kelas lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -10,5 +10,24 @@ public:
 };
 
 class joko : public seseorang {
+// virtual void pesan() = 0;
+  virtual void pesan() {
+    cout << "Pesan dari seseorang" << endl;
+  }
+};
+
+class joko : public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia : public seseorang {
 
 };
+
+int main(){
+
+}
+
